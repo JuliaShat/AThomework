26a433088c7ab5f1032870988884fa7ab9316902
JuliaShat <78312804+JuliaShat@users.noreply.github.com>
2021-02-14 20:13:03 +0200

    Update Functions.java

--- Functions.java
+++ Functions.java
@@ -90,5 +90,46 @@ public class Functions {
         }
     }
     
+     public static int getNumberFromString(String number) {
+        String[] UNITS = {"ноль","один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"};
+        String[] UNITS11_19 = {"одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать",
+                "семнадцать", "восемнадцать", "девятнадцать"};
+        String[] TENS = {"ноль", "десять", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят",
+                "восемьдесят", "девяносто"};
+        String[] HUNDREDS = {"сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот", "семьсот",
+                "восемьсот", "девятьсот"};
+
+        String lowerCase = number.toLowerCase().trim().replaceAll("\\s+", " ");
+        String[] splitNumber = lowerCase.split(" ");
+
+        if (splitNumber[0].equals("ноль")) {
+            return 0;
+        }
+
+        int convertedNumber = 0;
+        int wordsCounter = splitNumber.length;
+        while (wordsCounter != 0) {
+            for (int i = 0; i < UNITS.length-1 && wordsCounter != 0; i++) {
+                if (splitNumber[splitNumber.length - wordsCounter].equals(HUNDREDS[i])) {
+                    convertedNumber += (i + 1) * 100;
+                    wordsCounter--;
+
+                } else if (splitNumber[splitNumber.length - wordsCounter].equals(TENS[i])) {
+                    convertedNumber += i * 10;
+                    wordsCounter--;
+
+                } else if (splitNumber[splitNumber.length - wordsCounter].equals(UNITS[i])) {
+                    convertedNumber += i;
+                    wordsCounter--;
+
+                } else if (splitNumber[splitNumber.length - wordsCounter].equals(UNITS11_19[i])) {
+                    convertedNumber += i + 1;
+                    wordsCounter--;
+
+                }
+            }
+
+        }return convertedNumber;
+    }
     
 }
